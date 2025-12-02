35bf1b9781302e332e57e305c445825815b56a20
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-02 15:21:14 +0530

    The updated flex

--- src/hello-world/hello-world-style.js
+++ src/hello-world/hello-world-style.js
@@ -2,11 +2,16 @@ import styled from "styled-components";
 export const HelloWorldStyle = styled.div`
   .parent {
     display: flex;
+    flex-direction: column;
     background-color: green;
     //align-items: flex-start;
     // align-items: center;
-    align-items: flex-start;
-    justify-content: flex-end;
+    // align-items: flex-start;
+    //justify-content: flex-end;
+    align-items: flex-end;
+    //justify-content: space-evenly;
+    //justify-content: space-between;
+    //  justify-content: space-around;
     gap: 20px;
     height: 100vh;
   }
