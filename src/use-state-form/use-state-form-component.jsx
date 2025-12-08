a00ae0380905d25442d80f54e02b28fecc8737c7
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-08 15:33:04 +0530

    Form is pushed

--- src/use-state-form/use-state-form-component.jsx
+++ src/use-state-form/use-state-form-component.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
 const UsestateformComponent = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const fun1 = (e) => {
+  //   if (e.target.name === "email") {
+  //     setEmail(e.target.value);
+  //   } else if (e.target.name === "name") {
+  //     setName(e.target.value);
+  //   }
+  // };
+  // const submitthings = () => {
+  //   console.log("The name is :", name);
+  //   console.log("The email is", email);
+  // };
+  const [details, setDetails] = useState({ name: "", email: "" });
   const fun1 = (e) => {
-    if (e.target.name === "email") {
-      setEmail(e.target.value);
-    } else if (e.target.name === "name") {
-      setName(e.target.value);
-    }
+    setDetails({ ...details, [e.target.name]: e.target.value });
   };
   const submitthings = () => {
-    console.log("The name is :", name);
-    console.log("The email is", email);
+    console.log("The details:", details);
   };
   return (
     <div>
       <h1>Use State Form Component</h1>
       <form action={submitthings}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={fun1} />
+        <input type="text" id="Name" name="name" onChange={fun1} />
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" onChange={fun1} />
         <button type="submit">Submit</button>
