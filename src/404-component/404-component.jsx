92d62a2d6d63b5c972e461e0c178ad5fcf464a7c
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-08 11:55:21 +0530

    Link is added

--- src/404-component/404-component.jsx
+++ src/404-component/404-component.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const FourOFourComponent = () => {
-  return <h1>404 Component</h1>;
+  return (
+    <>
+      <h1>404 Component</h1>
+      <Link to="/leetcode/:1">Go to Home Page</Link>
+    </>
+  );
 };
 export default FourOFourComponent;
